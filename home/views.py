52f46096682b8fdff7f11df32b7a8eb59dc303e0
mayasaffron <mayasaffronhan@hotmail.com>
2021-02-17 15:24:29 +0000

    update: installed crispy forms and updated settings, urls and views

--- home/views.py
+++ home/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from .forms import ContactForm
+from django.conf import settings
+from django.core.mail import send_mail, BadHeaderError
 
 
-# Create your views here.
 def index(request):
     return render(request, 'home/index.html')
+
+
+def contactform(request):
+    """
+    Send an email to the admin
+    when site visitors send message via contact form
+    """
+    contact_form = ContactForm(request.POST)
+    if contact_form.is_valid():
+        name = contact_form.cleaned_data['name']
+        email = contact_form.cleaned_data['email']
+        subject = contact_form.cleaned_data['subject']
+        message = contact_form.cleaned_data['message']
+        try:
+            send_mail(
+                f"You've got a message from {name} ({email}) on contact form.",
+                f"Subject: {subject}, Message: {message}",
+                email,
+                [settings.DEFAULT_FROM_EMAIL],
+            )
+        except BadHeaderError:
+            return HttpResponse('Invalid header found.')
+
+    return redirect('thankyou_page')
--- kaur_health/settings.py
+++ kaur_health/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
+
+    #Other 
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -56,6 +59,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'kaur_health.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -73,6 +78,10 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field'
+            ]
         },
     },
 ]
