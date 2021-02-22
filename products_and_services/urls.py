7b4afe403c575b191af6d38fa285453a0ed02c5f
mayasaffron <mayasaffronhan@hotmail.com>
2021-02-22 18:42:38 +0000

    created: created the products and services view and template, updated the urls and css

---
+++ products_and_services/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.all_items, name='all_items'),
+]
--- products_and_services/views.py
+++ products_and_services/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Service, Category
 
-# Create your views here.
+
+def all_items(request):
+    products = Product.objects.all()
+    services = Service.objects.all()
+
+    context = {
+        'products': products,
+        'services': services,
+    }
+    return render(request, 'products_and_services/all_items.html', context)
