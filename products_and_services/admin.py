79f8506780f089a04573d99cd5525152b95276cd
mayasaffron <mayasaffronhan@hotmail.com>
2021-02-22 13:53:17 +0000

    Update: completed the services and products fixtures and loaded them to admin

--- products_and_services/admin.py
+++ products_and_services/admin.py
@@ -1,5 +1,38 @@
 from django.contrib import admin
-from .models import Category, Product
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+from .models import Category, Product, Service
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'description',
+        'category',
+        'price',
+        'product_image',
+        'rating',
+        )
+    ordering = ('name', 'price')
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'description',
+        'category',
+        'price',
+        'product_image',
+        'rating',
+        )
+    ordering = ('name', 'price')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+        )
+
+
+admin.site.register(Service, ServiceAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
