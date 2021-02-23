8e174f66b1248ee2f0070bb0b319487d5b137686
mayasaffron <mayasaffronhan@hotmail.com>
2021-02-23 19:45:47 +0000

    create: built product and service detail views, service details not yet functioning correctly

--- products_and_services/urls.py
+++ products_and_services/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.all_items, name='all_items'),
+    path('<int:product_id>/',
+         views.product_detail, name='product_detail'),
+    path('<int:service_id>/',
+         views.service_detail, name='service_detail'),
 ]
--- products_and_services/views.py
+++ products_and_services/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product, Service, Category
 
 
@@ -10,4 +12,25 @@ def all_items(request):
         'products': products,
         'services': services,
     }
+
     return render(request, 'products_and_services/all_items.html', context)
+
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+    return render(request, 'products_and_services/product_detail.html',
+                  context)
+
+
+def service_detail(request, service_id):
+    service = get_object_or_404(Service, pk=service_id)
+
+    context = {
+        'service': service,
+    }
+    return render(request, 'products_and_services/service_detail.html',
+                  context)
