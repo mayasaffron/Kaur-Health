8aba97404cedef107e7df51b6eba084ee7aa98d7
mayasaffron <mayasaffronhan@hotmail.com>
2021-02-25 17:25:11 +0000

    update: added add to bag functionality

--- bag/views.py
+++ bag/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
 
-# Create your views here.
+from products_and_services.models import Product, Service
+
+
+def view_bag(request):
+    return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    '''Add a quantity of the specified product to the shopping bag'''
+    product = Product.objects.get(pk=item_id)
+    service = Service.objects.get(pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+        messages.success(request,
+                         f' Updated {product.name} {service.name}'
+                         'quantity to {bag[item_id]}')
+    else:
+        bag[item_id] = quantity
+        messages.success(request,
+                         f' {product.name} {service.name}'
+                         ' added to your bag! ')
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
--- kaur_health/settings.py
+++ kaur_health/settings.py
@@ -79,6 +79,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents'
             ],
             'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
@@ -162,3 +163,12 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# Stripe
+FREE_DELIVERY_THRESHOLD = 30
+STANDARD_DELIVERY_PERCENTAGE = 10
+# STRIPE_CURRENCY = 'usd'
+# STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+# STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+# STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
