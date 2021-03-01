9ff0b794f8aa385f1526523af1613271cf928f7e
mayasaffron <mayasaffronhan@hotmail.com>
2021-03-01 16:51:44 +0000

    bug: attempting to solve bug, by defining item_type, issue still unresolved

--- bag/views.py
+++ bag/views.py
@@ -8,26 +8,31 @@ def view_bag(request):
     return render(request, 'bag/bag.html')
 
 
-def add_product_to_bag(request, item_id):
+def add_product_to_bag(request, item_id, self, product_id):
     '''Add a quantity of the specified product to the shopping bag'''
     product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
+    item_type = get_object_or_404(Product, pk=product_id)
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
-        messages.success(request,
-                         f' Updated {product.name}'
-                         f'quantity to {bag[item_id]}')
+    if item_type:
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+            messages.success(request,
+                             f' Updated {product.name}'
+                             f'quantity to {bag[item_id]}')
+        else:
+            bag[item_id] = quantity
+            messages.success(request,
+                             f' {product.name}'
+                             ' added to your bag! ')
+
+        request.session['bag'] = bag
+        return redirect(redirect_url)
     else:
-        bag[item_id] = quantity
-        messages.success(request,
-                         f' {product.name}'
-                         ' added to your bag! ')
-
-    request.session['bag'] = bag
-    return redirect(redirect_url)
+        messages.error(request, 'could not add this to your bag')
+        return redirect(redirect_url)
 
 
 def add_service_to_bag(request, item_id):
