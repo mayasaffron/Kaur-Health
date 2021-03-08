151868c3ecb7c6a2e2ebb34594121f86f9e01721
mayasaffron <mayasaffronhan@hotmail.com>
2021-03-08 14:49:15 +0000

    fix: added an extra quantity field in order line item class and _product/service to distinguish

--- checkout/models.py
+++ checkout/models.py
@@ -67,9 +67,10 @@ class OrderLineItem(models.Model):
                               related_name='lineitems')
     product = models.ForeignKey(Product, null=False,
                                 blank=False, on_delete=models.CASCADE)
+    quantity_product = models.IntegerField(null=False, blank=False, default=0)
     service = models.ForeignKey(Service, null=False,
                                 blank=False, on_delete=models.CASCADE)
-    quantity = models.IntegerField(null=False, blank=False, default=0)
+    quantity_service = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6,
                                          decimal_places=2, null=False,
                                          blank=False, editable=False)
