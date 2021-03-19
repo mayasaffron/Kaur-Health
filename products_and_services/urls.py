41883172cced78d818f6ec98d899d46d27eb6362
mayasaffron <mayasaffronhan@hotmail.com>
2021-03-19 11:32:54 +0000

    update: added UD from CRUD, functionality

--- products_and_services/urls.py
+++ products_and_services/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('', views.all_items, name='all_items'),
     path('product_detail/<int:product_id>/',
          views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
+    path('update/<int:product_id>/', views.update_product,
+         name='update_product'),
+    path('delete/<int:product_id>/', views.delete_product,
+         name='delete_product'),
 ]
--- products_and_services/views.py
+++ products_and_services/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.db.models.functions import Lower
 from django.db.models import Q
 from .models import Product, Category
+from .forms import ProductForm
 
 
 def all_items(request):
@@ -78,3 +80,72 @@ def product_detail(request, product_id):
     }
     return render(request, 'products_and_services/product_detail.html',
                   context)
+
+
+@login_required
+def add_product(request):
+    '''A view to allow super users to add products'''
+    if not request.user.is_superuser:
+        messages.error(request, 'sorry only store owners can add products')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Product not added, please recheck the form')
+            return redirect(reverse('add_product'))
+    else:
+        form = ProductForm()
+
+    template = 'products_and_services/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def update_product(request, product_id):
+    '''A view to allow super users to edit products'''
+    if not request.user.is_superuser:
+        messages.error(request, 'sorry only store owners can update products')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update, please recheck the form')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products_and_services/update_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_product(request, product_id):
+    '''A view to allow super users to delete products'''
+    if not request.user.is_superuser:
+        messages.error(request, 'sorry only store owners can delete products')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, f' {product.name} has been deleted')
+    return redirect(reverse('all_items'))
