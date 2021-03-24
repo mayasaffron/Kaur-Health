7409d9c8e95ebd021b0a4cf1aad2583b8636fe75
mayasaffron <mayasaffronhan@hotmail.com>
2021-03-24 18:31:20 +0000

    create: made the blog model and form for CRUD

--- blog/admin.py
+++ blog/admin.py
@@ -3,3 +3,5 @@ from .models import BlogPost
 
 
 admin.site.register(BlogPost)
+
+
--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from products_and_services.models import Category
 
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=254)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, null=True,
+                                 blank=True, on_delete=models.SET_NULL)
     body = models.TextField()
+    blog_image = models.ImageField(null=False, blank=True)
 
     def __str__(self):
-        return self.title + '|' + self.author
+        return self.title + '|' + str(self.author)
