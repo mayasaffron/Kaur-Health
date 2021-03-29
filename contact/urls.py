873dd150d3c14818ae9066b9710cd75647feb6f4
mayasaffron <mayasaffronhan@hotmail.com>
2021-03-29 16:02:57 +0000

    delete: removed contact app and moved relevant code to home app

--- contact/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.contactform, name='contactform'),
-    path('contact_response/', views.contact_response, name='contact_response'),
-]
