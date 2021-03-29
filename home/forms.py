e12346b2033099c41cbb6272cd1472a9b488a4b7
mayasaffron <mayasaffronhan@hotmail.com>
2021-03-29 16:01:06 +0000

    update: made front end changes to contact form

---
+++ home/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+
+
+class ContactForm(forms.Form):
+    """
+    Contact Form for contact link
+    """
+    CATEGORY = (
+        ('NFP Inquiry',
+         'NFP Inquiry'),
+        ('DIY skincare Inquiry', 'DIY skincare Inquiry'),
+        ('Female health Inquiry', 'Female health Inquiry'),
+        ('Other', 'Other'),
+    )
+
+    name = forms.CharField(required=True, label="Name", widget=forms.TextInput())
+    email = forms.EmailField(required=True, label="Email", widget=forms.EmailInput())
+    subject = forms.CharField(
+        label="Subject",
+        widget=forms.Select(choices=CATEGORY),
+    )
+    message = forms.CharField(
+        label="Message",
+        widget=forms.Textarea(attrs={
+            "rows": 7,
+        })
+    )
+
+    class Meta:
+        fields = ['name', 'email', 'subject', 'message']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
